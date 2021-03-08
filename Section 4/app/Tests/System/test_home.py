e71166f62b8e9c5a2dc7b0c95db6affdea1fbabd
Kaloyan <kaloyanboichev@abv.bg>
2021-03-08 13:25:57 +0200

    adding base_test and test_home in section4

---
+++ Section 4/app/Tests/System/test_home.py
@@ -0,0 +1,11 @@
+from tests.system.base_test import BaseTest
+import json
+
+
+class TestHome(TestCase):
+    def test_home(self):
+        with app.test_client() as c:
+            resp = c.get('/')
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(json.loads(resp.get_data()), {'message': 'Hello world!'})
